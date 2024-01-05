edfbc96ea643910931766c36556ac7e5c1190e99
e.grebenev <e.grebenev@space307.com>
2024-01-06 02:34:28 +0300

    smarthouse_5

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,21 @@ use otus_smarthouse3::DynamicSmartDevice;
 fn main() {
     let mut house = otus_smarthouse3::SmartHouse::default();
     let room = "kitchen";
-    house.add_room(room);
-    house.add_device(
-        room,
-        Box::new(otus_smarthouse3::SmartThermometer::new("thermo")) as DynamicSmartDevice,
-    );
-    house.add_device(
-        room,
-        Box::new(otus_smarthouse3::SmartSocket::new("socket")) as DynamicSmartDevice,
-    );
+    house.add_room(room).unwrap();
+    let err = house.add_room(room).unwrap_err();
+    println!("there is printed error: {}", err);
+    house
+        .add_device(
+            room,
+            Box::new(otus_smarthouse3::SmartThermometer::new("thermo")) as DynamicSmartDevice,
+        )
+        .unwrap();
+    house
+        .add_device(
+            room,
+            Box::new(otus_smarthouse3::SmartSocket::new("socket")) as DynamicSmartDevice,
+        )
+        .unwrap();
 
     println!("{}", house.create_full_report());
 }
--- src/smarthouse.rs
+++ src/smarthouse.rs
@@ -1,6 +1,55 @@
+use std::collections::hash_map::Entry;
 use std::collections::HashMap;
+use std::error::Error;
+use std::fmt;
 
 use crate::DynamicSmartDevice;
+
+#[derive(Debug)]
+pub struct RoomAlreadyExistsError {
+    room_name: String,
+}
+
+impl fmt::Display for RoomAlreadyExistsError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Room already exists: {}", self.room_name)
+    }
+}
+
+impl Error for RoomAlreadyExistsError {}
+
+#[derive(Debug)]
+pub struct NoSuchRoomError {
+    room_name: String,
+}
+
+impl fmt::Display for NoSuchRoomError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "no such room: {}", self.room_name)
+    }
+}
+
+impl Error for NoSuchRoomError {}
+
+#[derive(Debug)]
+pub struct NoSuchDeviceError {
+    device_name: String,
+}
+
+impl fmt::Display for NoSuchDeviceError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "no such device: {}", self.device_name)
+    }
+}
+
+impl Error for NoSuchDeviceError {}
+
+#[derive(Debug)]
+pub enum SmartHouseError {
+    RoomExists(RoomAlreadyExistsError),
+    RoomNotFound(NoSuchRoomError),
+    DeviceNotFound(NoSuchDeviceError),
+}
 
 #[derive(Default)]
 pub struct SmartHouse {
@@ -8,14 +57,32 @@ pub struct SmartHouse {
 }
 
 impl SmartHouse {
-    pub fn add_room(&mut self, name: &str) {
-        if !self.data.contains_key(name) {
-            self.data.insert(name.to_string(), Vec::new());
+    pub fn add_room(&mut self, name: &str) -> Result<(), RoomAlreadyExistsError> {
+        match self.data.entry(name.to_string()) {
+            Entry::Vacant(e) => {
+                e.insert(vec![]);
+                Ok(())
+            }
+            Entry::Occupied(_) => Err(RoomAlreadyExistsError {
+                room_name: name.to_string(),
+            }),
         }
     }
 
-    pub fn add_device(&mut self, room: &str, device: DynamicSmartDevice) {
-        self.data.get_mut(room).unwrap().push(device);
+    pub fn add_device(
+        &mut self,
+        room: &str,
+        device: DynamicSmartDevice,
+    ) -> Result<(), NoSuchRoomError> {
+        match self.data.get_mut(room) {
+            None => Err(NoSuchRoomError {
+                room_name: room.to_string(),
+            }),
+            Some(room) => {
+                room.push(device);
+                Ok(())
+            }
+        }
     }
 
     #[allow(dead_code)]
@@ -24,10 +91,12 @@ impl SmartHouse {
     }
 
     #[allow(dead_code)]
-    pub fn devices(&self, room: &str) -> Option<&Vec<DynamicSmartDevice>> {
+    pub fn devices(&self, room: &str) -> Result<&Vec<DynamicSmartDevice>, NoSuchRoomError> {
         match self.data.get(room) {
-            Some(devices_vec) => Some(devices_vec),
-            None => None,
+            Some(devices_vec) => Ok(devices_vec),
+            None => Err(NoSuchRoomError {
+                room_name: room.to_string(),
+            }),
         }
     }
 
@@ -42,18 +111,27 @@ impl SmartHouse {
         result
     }
 
-    pub fn get_device_info(&self, room: &str, device_name: &str) -> String {
+    pub fn get_device_info(
+        &self,
+        room: &str,
+        device_name: &str,
+    ) -> Result<String, SmartHouseError> {
         match self.data.get(room) {
             Some(devices_vec) => {
                 let device = devices_vec.iter().find(|d| {
                     return d.get_name() == device_name;
                 });
                 match device {
-                    Some(d) => std::format!("it is report for [{d}] in room [{room}]"),
-                    None => "no such device".to_string(),
+                    Some(d) => Ok(std::format!("it is report for [{d}] in room [{room}]")),
+                    // None =>  Err(SmartHouseError::DeviceNotFound{ 0: NoSuchDeviceError {device_name:device_name.to_string()}}),
+                    None => Err(SmartHouseError::DeviceNotFound(NoSuchDeviceError {
+                        device_name: device_name.to_string(),
+                    })),
                 }
             }
-            None => "No such room".to_string(),
+            None => Err(SmartHouseError::RoomNotFound(NoSuchRoomError {
+                room_name: room.to_string(),
+            })),
         }
     }
 }
@@ -69,16 +147,16 @@ mod tests {
         assert_eq!(house.get_rooms().len(), 0);
 
         let room = "chinese_room";
-        house.add_room(room);
+        house.add_room(room).unwrap();
         assert_eq!(house.get_rooms().len(), 1);
         assert_eq!(house.get_rooms()[0], room);
 
-        house.add_room(room);
+        house.add_room(room).unwrap_err();
         assert_eq!(house.get_rooms().len(), 1);
         assert_eq!(house.get_rooms()[0], room);
 
         let room2 = "kitchen";
-        house.add_room(room2);
+        house.add_room(room2).unwrap();
         assert_eq!(house.get_rooms().len(), 2);
         // TODO здесь точно проще нельзя проверить наличие строки в векторе?
         assert!(house.get_rooms().iter().any(|e| e == &room));
@@ -90,11 +168,13 @@ mod tests {
         let mut house = SmartHouse::default();
         let room = "chinese_room";
         let device_name = "thermo";
-        house.add_room(room);
-        house.add_device(
-            room,
-            Box::new(SmartThermometer::new(device_name)) as DynamicSmartDevice,
-        );
-        house.get_device_info(room, device_name);
+        house.add_room(room).unwrap();
+        house
+            .add_device(
+                room,
+                Box::new(SmartThermometer::new(device_name)) as DynamicSmartDevice,
+            )
+            .unwrap();
+        house.get_device_info(room, device_name).unwrap();
     }
 }
